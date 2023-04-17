8f36c12199fef0466324376c8de86ead18f1ec2a
huyhuanhg <huyhuanhg@gmail.com>
2023-04-18 01:39:56 +0700

    env

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -16,14 +16,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-console.log('firebaseConfig :>> ', firebaseConfig);
-console.log('process.env.FIREBASE_API_KEY :>> ', process.env.REACT_APP_FIREBASE_API_KEY);
-console.log('process.env.FIREBASE_AUTH_DOMAIN :>> ', process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
-console.log('process.env.FIREBASE_PROJECT_ID :>> ', process.env.REACT_APP_FIREBASE_PROJECT_ID);
-console.log('process.env.FIREBASE_STORAGE_BUCKET :>> ', process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
-console.log('process.env.FIREBASE_MESSAGING_SENDER_ID :>> ', process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
-console.log('process.env.TEST :>> ', process.env.TEST);
-
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
